d62ebcfdd1933f3402a232db9064341a94f07c63
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2023-09-01 01:58:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-indigo-600 text-2xl font-[500]">Hola mundo</h1>
+    </>
+  );
+}
+
+export default App;
